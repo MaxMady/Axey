b36efb2861ea793b5451ada953f585805a2c9030
MaxMady <kknbhcr56tfv78hygfh@outlook.com>
2022-09-06 13:37:15 +0530

    Added error catchers

--- Showdown/showoff.js
+++ Showdown/showoff.js
@@ -1,84 +1,118 @@
 require("puppeteer");
 const puppeteer = require("puppeteer-extra");
 const { AttachmentBuilder } = require("discord.js");
-const e = require('./pageManager.js').e
+const e = require("./pageManager.js").e;
 
 const { WebhookClient, EmbedBuilder } = require("discord.js");
-const {QuickDB} = require('quick.db')
-const db = new QuickDB()
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 async function start(url, interaction) {
   console.log(url);
   let id = url.split("/")[3].split("-").join("-");
   e(async (page, browser, index) => {
-  await page.goto(url, { waitUntil: "networkidle0" });
-  setTimeout(async function () {
-    let w1 = 0;
+    try {
+      await page.goto(url, { waitUntil: "networkidle0" });
+    } catch (Err) {
+      console.log(Err);
+      await interaction.followUp({
+        content: `An error occured while loading the site!`,
+        ephemeral: true,
+      });
+    }
+    setTimeout(async function () {
+      let w1 = 0;
 
-    console.log(`Connected to websocket`);
-    await interaction.followUp(`Connected to websocket!`)
-    
-    page.on("console", (message) => {
-      format(message.text(), page, browser, w1, interaction, url, index);
-    });
-  }, 3000);
-})
+      console.log(`Connected to websocket`);
+      await interaction.followUp(`Connected to websocket!`);
+      try {
+        page.on("console", (message) => {
+          format(message.text(), page, browser, w1, interaction, url, index);
+        });
+      } catch (Err) {
+        await interaction.followUp({
+          content: `WS was closed when recieving request...`,
+          ephemeral: true,
+        });
+        console.log(Err);
+      }
+    }, 3000);
+  });
 }
 let laturn = `1`;
 async function format(msg, page, browser, w1, interaction, url, index) {
-  if(!index) index = 0;
+  if (!index) index = 0;
+  try {
     const title = await page.title();
-  let m = msg.split('\n')
+  } catch (err) {
+    console.log(err);
+    await interaction.followUp({
+      content: `Unable to get usernames`,
+      ephemeral: true,
+    });
+  }
+  let m = msg.split("\n");
   m.forEach(async (ai) => {
-    if ((
+    if (
       ai.includes("win") &&
       !ai.includes("Wyrmwind") &&
-      !ai.includes("Tailwind"))) {
-        if(ai.includes(`p1:`) || ai.includes(`p2:`)) return;
+      !ai.includes("Tailwind")
+    ) {
+      if (ai.includes(`p1:`) || ai.includes(`p2:`)) return;
       console.log(`Battle has ended!`);
       w1++;
-      
+
       const embed = new EmbedBuilder()
         .setTitle(`${title}`)
         .setColor("Green")
         .setDescription(`${ai.split("|")[2]} has won the battle!`);
-        let tip = Math.random()*10
-        if(tip > 8) embed.setFooter({ text: `An awesome bot by Dux#2925` })
-      interaction.channel
-        .send({ embeds: [embed] })
-        .then(async (e) => {
-          let pagg = await db.get('page')
-          if(pagg === null) pagg = 1;
-          pagg--;
-          if(pagg === 0) {
-            await db.delete(`browser`)
+      let tip = Math.random() * 10;
+      if (tip > 6) embed.setFooter({ text: `An awesome bot by Dux#2925` });
+      interaction.channel.send({ embeds: [embed] }).then(async (e) => {
+        let pagg = await db.get("page");
+        if (pagg === null) pagg = 1;
+        pagg--;
+        try {
+          if (pagg === 0) {
+            await db.delete(`browser`);
             await browser.close();
           } else {
             await page.close();
           }
-          await db.set(`page`, pagg)
-        });
-    } else if( ai.includes('forfeited.')) {
+        } catch (err) {
+          console.log(err);
+          await interaction.followUp({
+            content: `An error occured while closing websocket`,
+            ephemeral: true,
+          });
+        }
+        await db.set(`page`, pagg);
+      });
+    } else if (ai.includes("forfeited.")) {
       w1++;
       const embed = new EmbedBuilder()
         .setTitle(`${title}`)
         .setColor("Green")
-        .setDescription(`${ai.split("|")[2].split('forfieted.')[0]} has forfeited! The battle has ended...`);
-        let tip1 = Math.random()*10
-        if(tip1 > 8) embed.setFooter({ text: `An awesome bot by Dux#2925` })
+        .setDescription(
+          `${
+            ai.split("|")[2].split("forfieted.")[0]
+          } has forfeited! The battle has ended...`
+        );
+      let tip1 = Math.random() * 10;
+      if (tip1 > 6) embed.setFooter({ text: `An awesome bot by Dux#2925` });
       interaction.channel.send({ embeds: [embed] }).then(async (e) => {
-          let pagg = await db.get('page')
-          if(pagg === null) pagg = 1;
-          pagg--;
-          if(pagg === 0) {
-            await db.delete(`browser`)
-            await browser.close();
-          }
-          await db.set(`page`, pagg)
-        });
+        let pagg = await db.get("page");
+        if (pagg === null) pagg = 1;
+        pagg--;
+        if (pagg === 0) {
+          await db.delete(`browser`);
+          await browser.close();
+        }
+        await db.set(`page`, pagg);
+      });
     }
   });
 
-  console.log(m)
+  console.log(m);
   let id = url.split("/")[3].split("-").join("-");
   let str1 = ``,
     str2 = ``,
@@ -142,12 +176,14 @@ async function format(msg, page, browser, w1, interaction, url, index) {
           sen[4]
         } stage(s)!\n`;
       } else if (e.includes("-damage")) {
-        let hp = sen[3].split("/")[0]
-        if(hp === `0 fnt`){ hp = `0% [FAINTED]`} else { hp += `%` }
+        let hp = sen[3].split("/")[0];
+        if (hp === `0 fnt`) {
+          hp = `0% [FAINTED]`;
+        } else {
+          hp += `%`;
+        }
         if (sen.length > 4) {
-          str1 += `${hp} was hurt by ${
-            sen[4].split(" ")[2]
-          }. HP: \`${hp}\`\n`;
+          str1 += `${hp} was hurt by ${sen[4].split(" ")[2]}. HP: \`${hp}\`\n`;
         } else {
           str1 += `${sen[2].split(":")[1]} lost some HP! \`HP: ${hp}\`\n`;
         }
@@ -218,8 +254,12 @@ async function format(msg, page, browser, w1, interaction, url, index) {
           sen[4]
         } stage(s)!\n`;
       } else if (e.includes("-damage")) {
-        let hp = sen[3].split("/")[0]
-          if(hp === `0 fnt`){ hp = `0% [FAINTED]`} else { hp += `%` }
+        let hp = sen[3].split("/")[0];
+        if (hp === `0 fnt`) {
+          hp = `0% [FAINTED]`;
+        } else {
+          hp += `%`;
+        }
         if (sen.length > 4) {
           str2 += `${sen[2].split(":")[1]} was hurt by ${
             sen[4].split(" ")[2]
@@ -236,7 +276,7 @@ async function format(msg, page, browser, w1, interaction, url, index) {
       } else if (e.includes("-sidestart")) {
         str2 += `${sen[3]} were placed on the battle field!\n`;
       } else if (e.includes("-status")) {
-        let poke = sen[1].split(' ').splice(0, 1);
+        let poke = sen[1].split(" ").splice(0, 1);
         if (e.includes(`psn`)) {
           str2 += `${sen.join(" ")} was poisoned!\n`;
         } else if (e.includes("slp")) {
@@ -307,8 +347,8 @@ async function format(msg, page, browser, w1, interaction, url, index) {
       { name: `${n1}`, value: `${str1}`, inline: true },
       { name: `${n2}`, value: `${str2}`, inline: true },
     ]);
-    let tip2 = Math.random()*10
-   if(tip2 > 8) embed.setFooter({ text: `An awesome bot by Dux#2925` })
+  let tip2 = Math.random() * 10;
+  if (tip2 > 6) embed.setFooter({ text: `An awesome bot by Dux#2925` });
   if (str3 != ``) {
     embed.setDescription(`${str3}`);
   }
@@ -316,9 +356,9 @@ async function format(msg, page, browser, w1, interaction, url, index) {
   if (w1 != 0) return;
   interaction.channel.send({ embeds: [embed] }).then(async (e) => {
     let id = url.split("/")[3].split("-").join("-");
-    let img = await page.$(`#room-${id} > div.battle > div > div:nth-child(3)`);
-    e.embeds[0];
     try {
+      let img = await page.$(`#room-${id} > div.battle > div > div:nth-child(3)`);
+    e.embeds[0];
       await page.waitForSelector(
         `#room-${id} > div.battle-controls > p:nth-child(1) > button:nth-child(5)`
       );
@@ -329,7 +369,8 @@ async function format(msg, page, browser, w1, interaction, url, index) {
         path: `Battle_Images/battle_${index}.png`,
       });
     } catch (err) {
-      console.log(`Error: 347`);
+      console.log(`Unable to get image!`);
+      await interaction.followUp({content: `Unable to load image!`, ephemeral: true})
     }
     const file = new AttachmentBuilder(`./Battle_Images/battle_${index}.png`);
     let receivedEmbed = e.embeds[0];
@@ -338,7 +379,6 @@ async function format(msg, page, browser, w1, interaction, url, index) {
     );
     await e.edit({ embeds: [exampleEmbed], files: [file] });
   });
-  
 }
 
 module.exports = start;
